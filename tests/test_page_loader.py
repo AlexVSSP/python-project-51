965ec07019a3025750d594687ab2bd5d0832ccb7
Aleksei Seleznev <ferfa2@rambler.ru>
2022-11-25 19:21:45 +0300

    add tests for the new fixtures

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,23 +1,76 @@
 import pytest
 import os
 import requests_mock
-from page_loader import download
 import tempfile
+from page_loader import download, download_image, make_dir_name
+
+
+# @pytest.fixture
+# def nodejs_page_content():
+#     with open("tests/fixtures/nodejs_page.html", 'rb') as f:
+#         return f.read()
+#
+#
+# def test_download_path_to_html_nodejs(nodejs_page_content):
+#     with tempfile.TemporaryDirectory() as tmpdir:
+#         url = 'https://page-loader.hexlet.repl.co/'
+#         with requests_mock.Mocker() as m:
+#             m.get(url, content=nodejs_page_content)
+#             download(url, tmpdir)
+#         # path = f'{tmpdir}/page-loader-hexlet-repl-co-.html'
+#         path = os.path.join(tmpdir, 'page-loader-hexlet-repl-co-.html')
+#         assert os.path.exists(path)
+#         with open(path, 'rb') as f:
+#         # print(f.read().decode("utf-8"))
+#         # print(nodejs_page_content.decode("utf-8"))
+#             assert f.read() == nodejs_page_content
 
 
 @pytest.fixture
-def page_content():
-    with open("tests/fixtures/page_code.html") as f:
+def govuk_page_content():
+    with open("tests/fixtures/govuk_page.html", 'rb') as f:
         return f.read()
 
 
-def test_download_path_to_html(page_content):
+def test_download_path_to_html_govuk(govuk_page_content):
     with tempfile.TemporaryDirectory() as tmpdir:
-        url = 'https://ru.hexlet.io/courses'
+        url = 'https://www.gov.uk'
         with requests_mock.Mocker() as m:
-            m.get(url, text=page_content)
+            m.get(url, content=govuk_page_content)
             download(url, tmpdir)
-        path = f'{tmpdir}/ru-hexlet-io-courses.html'
+        path = os.path.join(tmpdir, 'www-gov-uk.html')
         assert os.path.exists(path)
-        with open(path) as f:
-            assert f.read() == page_content
+        with open(path, 'rb') as f:
+            assert f.read() == govuk_page_content
+
+
+def test_create_dir():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        url = 'https://www.gov.uk'
+        with requests_mock.Mocker() as m:
+            m.get(url)
+            download(url, tmpdir)
+        # correct_dir = f'{tmpdir}/www-gov-uk_files'
+        expect_dir = os.path.join(tmpdir, 'www-gov-uk_files')
+        assert os.path.isdir(expect_dir)
+
+
+@pytest.fixture
+def nodejs_page_content():
+    with open("tests/fixtures/page-loader-hexlet-repl-co-assets-professions-nodejs.png", 'rb') as f:
+        return f.read()
+
+
+def test_get_nodejs_image(nodejs_page_content):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        url = 'https://page-loader.hexlet.repl.co/'
+        link_path = '/assets/professions/nodejs.png'
+        dir_name = make_dir_name(url)
+        dir_path = os.path.join(tmpdir, dir_name)
+        os.mkdir(dir_path)
+        with requests_mock.Mocker() as m:
+            m.get(link_path, content=nodejs_page_content)
+            download_image(url, dir_path, link_path)  # Think about to make abspath to the second arg
+        expect_dir_path = os.path.join(tmpdir, 'page-loader-hexlet-repl-co-_files')
+        expect_image_path = os.path.join(expect_dir_path, 'page-loader-hexlet-repl-co-assets-professions-nodejs.png')
+        assert os.path.isfile(expect_image_path)
