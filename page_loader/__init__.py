d7bb5de243d67edfaac1269f8b0c893e7cddb8ac
Aleksei Seleznev <ferfa2@rambler.ru>
2022-12-05 21:52:24 +0300

    add functions from module "names"

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -1,6 +1,9 @@
-from page_loader.download import download, download_image, \
-    make_dir_name, make_file_name_image_asset
+from page_loader.download import download, download_image
+# make_file_name_image_asset
+# make_dir_name
+from page_loader.names import make_dir_name, make_file_name_html
 
 
-__all__ = ('download', 'download_image', 'make_dir_name',
-           'make_file_name_image_asset')
+__all__ = ('download', 'download_image',
+           'make_dir_name', 'make_file_name_html')
+# 'make_file_name_image_asset')
