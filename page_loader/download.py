baee1951bea0a72735c7c92eda2e8aafdd683909
Aleksei Seleznev <ferfa2@rambler.ru>
2022-12-05 21:52:54 +0300

    add check exceptions

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,109 +1,67 @@
 import os
 import os.path
 import requests
-import re
+# import re
+import logging
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse, urljoin
+# from urllib.parse import urlparse, urljoin
+from page_loader.names import make_file_name_html, make_dir_name
+# make_file_name_image_asset, make_file_name_image_https
+from page_loader.download_recouces import download_image
 
 
-def make_file_name_html(url):
-    if url.endswith('.html'):
-        url = url[:-5]
-    no_scheme_url = re.search(r'(?<=//).+', url)
-    url_with_dash_html = re.sub(r'[^\da-zA-Z]', '-', no_scheme_url[0])
-    url_with_extension_html = f'{url_with_dash_html}.html'
-    return url_with_extension_html
+py_logger = logging.getLogger(__name__)
+py_logger.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"{__name__}.log", mode='w')
+# py_handler.setLevel(logging.INFO)
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s "
+                                 "%(message)s")
+
+py_handler.setFormatter(py_formatter)
+py_logger.addHandler(py_handler)
 
 
-def make_dir_name(url):
-    if url.endswith('.html'):
-        url = url[:-5]
-    no_scheme_url = re.search(r'(?<=//).+', url)
-    dir_name_with_dash = re.sub(r'[^\da-zA-Z]', '-', no_scheme_url[0])
-    dir_name = f'{dir_name_with_dash}_files'
-    return dir_name
-
-
-def make_file_name_image_asset(url, link_path):
-    url_parse = urlparse(url)
-    url_netloc = url_parse.netloc
-    url_netloc_with_dash = re.sub(r'[^\da-zA-Z]', '-', url_netloc)
-
-    no_extension_link_path = os.path.splitext(link_path)[0]
-    link_path_with_dash = re.sub(r'[^\da-zA-Z]', '-', no_extension_link_path)
-    # print(url_netloc_with_dash)
-    # print(link_path_with_dash)
-    file_name = f'{url_netloc_with_dash}{link_path_with_dash}'
-    return file_name
-
-
-def make_file_name_image_https(link_path):
-    no_scheme_link_path = re.search(r'(?<=//).+', link_path)
-    # print(f'site= {no_scheme_link_path[0]}')
-    no_extension_link_path = os.path.splitext(no_scheme_link_path[0])
-    link_path_with_dash = re.sub(r'[^\da-zA-Z]', '-', no_extension_link_path[0])
-    file_name = link_path_with_dash
-    return file_name
-
-
-def download_image(url, dir_name, link_path):
-    # url_parse = urlparse(url)
-    link_path_parse = urlparse(link_path)
-    # Think about to make abspath to the second arg #
-    # if link_path.startswith('/assets'):
-    # print(link_path)
-    if link_path.startswith('/'):
-        # Make full image name
-        image_name = make_file_name_image_asset(url, link_path)
-        image_extension = os.path.splitext(link_path)[1]
-        if image_extension == '':
-            image_extension = '.html'
-        image_name_with_extension = f'{image_name}{image_extension}'
-        # Make image path in project
-        image_path = os.path.join(dir_name, image_name_with_extension)
-        # Make image name to change in HTML
-        asset_local = f'{dir_name}/{image_name_with_extension}'
-        # Make image path to download
-        # asset_link = f'{url}{link_path}'
-        asset_link = urljoin(url, link_path)
-        image = requests.get(asset_link)
-        # print(f"image - {image}")
-        with open(image_path, 'wb') as f:
-            f.write(image.content)
-        return asset_local
-
-    elif link_path.startswith(f'https://{link_path_parse.netloc}'):
-        # Make full image name
-        image_name = make_file_name_image_https(link_path)
-        image_extension = os.path.splitext(link_path)[1]
-        # print(f'image_extension = {image_extension}')
-        if image_extension == '':
-            image_extension = '.html'
-        image_name_with_extension = f'{image_name}{image_extension}'
-        image_path = os.path.join(dir_name, image_name_with_extension)
-        asset_local = f'{dir_name}/{image_name_with_extension}'
-        image = requests.get(link_path)
-        with open(image_path, 'wb') as f:
-            f.write(image.content)
-        return asset_local
-
-    else:
-        return link_path
-
-
+# flake8: noqa: C901
 def download(url, output=os.getcwd()):
+    py_logger.info(f"Module 'download' starts working")
+    py_logger.info(f"The following url is entered: {url}")
+    py_logger.info(f"Entered path to save a page: {output}")
+
+    if not os.access(output, os.W_OK):
+        py_logger.error(f"There are no write permissions in the specified directory: {output}")
+        raise PermissionError(f"There are no write permissions in the specified directory: {output}")
+
     # Making path to HTML file
     url_html = make_file_name_html(url)
     file_path = os.path.join(output, url_html)
+    if os.path.isfile(file_path):
+        py_logger.error(f"File with the specified name {file_path} already exists")
+        raise FileExistsError(f"File with the specified name {file_path} already exists")
+    py_logger.info(f"File location is on: {file_path}")
+
+    # # Creating a directory
+    # dir_name = make_dir_name(url)
+    # dir_path = os.path.join(output, dir_name)
+    # os.mkdir(dir_path)
+    # py_logger.info(f"Directory with resources is on: {dir_path}")
+
+    # Getting HTML file
+    response = requests.get(url)
+    if response.status_code != 200:
+        py_logger.error(f"Status code from {url} is {response.status_code}")
+        raise ConnectionError(f"Status code from {url} is {response.status_code}")
+    # print(f"Response is {response}")
+    file = response.text
 
     # Creating a directory
     dir_name = make_dir_name(url)
     dir_path = os.path.join(output, dir_name)
     os.mkdir(dir_path)
-
-    # Getting HTML file
-    response = requests.get(url)
-    file = response.text
+    if not os.path.isdir(dir_path):
+        py_logger.error(f"Specified directory {dir_path} is missing")
+        raise NotADirectoryError(f"Specified directory {dir_path} is missing")
+    py_logger.info(f"Directory with resources is on: {dir_path}")
 
     # Work with page
     # with open(file_path, 'w+') as fp:
@@ -115,17 +73,20 @@ def download(url, output=os.getcwd()):
             # /assets/....jpg  !!! DON'T FORGET TO MAKE A CHECK
             # IF NETLOC IN IMAGE AND SITE ARE THE SAME !!!
             link['src'] = download_image(url, dir_name, link_path)
-        # fp.write(str(soup.prettify()))
+            py_logger.info(f"Downloaded image has name: {link['src']}")
 
         for link in soup.find_all('link'):
             link_path = link.get('href')
             link['href'] = download_image(url, dir_name, link_path)
+            py_logger.info(f"Downloaded link has name: {link['href']}")
 
         for link in soup.find_all('script'):
             link_path = link.get('src')
             if link_path is not None:
                 link['src'] = download_image(url, dir_name, link_path)
+                py_logger.info(f"Downloaded script has name: {link['src']}")
 
         fp.write(soup.prettify())
 
+    py_logger.info(f"Result of 'download' function is on: {file_path}")
     return file_path
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 import os
 import argparse
+import logging
+import sys
+# import requests
+
 from page_loader import download
 
 
+script_logger = logging.getLogger(__name__)
+script_logger.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"{__name__}.log", mode='w')
+# py_handler.setLevel(logging.INFO)
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s "
+                                 "%(message)s")
+
+py_handler.setFormatter(py_formatter)
+script_logger.addHandler(py_handler)
+
+
+# flake8: noqa: C901
 def main():
     parser = argparse.ArgumentParser(prog='page-loader',
                                      description='Download page from web '
@@ -15,7 +32,59 @@ def main():
                         help='set path to save a page', default=os.getcwd())
     args = parser.parse_args()
 
-    print(download(args.URL, args.output))
+    script_logger.info(f'The command line utility "page-loader" is running')
+
+    try:
+        print(download(args.URL, args.output))
+
+    # except requests.exceptions.ConnectionError:
+    except ConnectionError:
+        print(f"Unable to connect to {args.URL}")
+        script_logger.error(f"Unable to connect to {args.URL}")
+        sys.exit(1)
+
+    except FileExistsError as file_exists_e:
+        print(file_exists_e)
+        script_logger.error(f"Error: {file_exists_e}")
+        sys.exit(1)
+
+    except FileNotFoundError as file_not_found_e:
+        print(file_not_found_e)
+        script_logger.error(f"Error: {file_not_found_e}")
+        sys.exit(1)
+
+    except InterruptedError as interrupted_e:
+        print(interrupted_e)
+        script_logger.error(f"Error: {interrupted_e}")
+        sys.exit(1)
+
+    except IsADirectoryError as is_a_directory_e:
+        print(is_a_directory_e)
+        script_logger.error(f"Error: {is_a_directory_e}")
+        sys.exit(1)
+
+    except NotADirectoryError as not_a_directory_e:
+        print(not_a_directory_e)
+        script_logger.error(f"Error: {not_a_directory_e}")
+        sys.exit(1)
+
+    except PermissionError as permission_e:
+        print(permission_e)
+        script_logger.error(f"Error: {permission_e}")
+        sys.exit(1)
+
+    except ProcessLookupError as process_look_up_e:
+        print(process_look_up_e)
+        script_logger.error(f"Error: {process_look_up_e}")
+        sys.exit(1)
+
+    except TimeoutError as timeout_e:
+        print(timeout_e)
+        script_logger.error(f"Error: {timeout_e}")
+        sys.exit(1)
+
+    else:
+        sys.exit(0)
 
 
 if __name__ == '__main__':
