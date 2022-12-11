7937858d7c2fb59841eb07f91c25d94b285dccbe
Aleksei Seleznev <ferfa2@rambler.ru>
2022-12-11 13:46:44 +0300

    change logic in download_resources function: return asset_local, instead of image_name

--- page_loader/download_recouces.py
+++ page_loader/download_recouces.py
@@ -2,7 +2,7 @@ import os.path
 import requests
 from urllib.parse import urlparse, urljoin
 from page_loader.names import make_file_name_image_asset, \
-    make_file_name_image_https
+    make_file_name_image_https, make_dir_name
 
 
 def download_image(url, dir_path, link_path):
@@ -15,6 +15,10 @@ def download_image(url, dir_path, link_path):
         if image_extension == '':
             image_extension = '.html'
         image_name_with_extension = f"{image_name}{image_extension}"
+
+        # Make image name to change in HTML
+        dir_name = make_dir_name(url)
+        asset_local = f"{dir_name}/{image_name_with_extension}"
 
         # Make image path in project
         image_path = os.path.join(dir_path, image_name_with_extension)
@@ -32,7 +36,7 @@ def download_image(url, dir_path, link_path):
             f.write(image.content)
         # return asset_local
         # return image_path
-        return image_name
+        return asset_local
 
     if link_path.startswith(f"https://{link_path_parse.netloc}"):
         # Make full image name
@@ -42,13 +46,18 @@ def download_image(url, dir_path, link_path):
             image_extension = '.html'
         image_name_with_extension = f"{image_name}{image_extension}"
         image_path = os.path.join(dir_path, image_name_with_extension)
+
+        # Make image name to change in HTML
+        dir_name = make_dir_name(url)
+        asset_local = f"{dir_name}/{image_name_with_extension}"
+
         # asset_local = f"{dir_name}/{image_name_with_extension}"
         image = requests.get(link_path)
         with open(image_path, 'wb') as f:
             f.write(image.content)
         # return asset_local
         # return image_path
-        return image_name
+        return asset_local
 
     else:
         return link_path
