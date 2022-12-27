ae5b8c526bcc3305ab8d705d10ae5766039f2f7f
Aleksei Seleznev <ferfa2@rambler.ru>
2022-12-27 18:42:19 +0300

    add separate module for raise errors

---
+++ page_loader/raise_errors.py
@@ -0,0 +1,38 @@
+import os
+import logging
+
+
+py_logger = logging.getLogger(__name__)
+py_logger.setLevel(logging.INFO)
+
+py_handler = logging.FileHandler(f"{__name__}.log", mode='w')
+py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s "
+                                 "%(message)s")
+
+py_handler.setFormatter(py_formatter)
+py_logger.addHandler(py_handler)
+
+
+# flake8: noqa: C901
+def permission_error(output):
+    if not os.access(output, os.W_OK):
+        py_logger.error(f"There are no write permissions in the specified directory: {output}")
+        raise PermissionError(f"There are no write permissions in the specified directory: {output}")
+
+
+def file_exists_error(file_path):
+    if os.path.isfile(file_path):
+        py_logger.error(f"File with the specified name {file_path} already exists")
+        raise FileExistsError(f"File with the specified name {file_path} already exists")
+
+
+def connection_error(url, response):
+    if response.status_code != 200:
+        py_logger.error(f"Status code from {url} is {response.status_code}")
+        raise ConnectionError(f"Status code from {url} is {response.status_code}")
+
+
+def not_a_directory_error(dir_path):
+    if not os.path.isdir(dir_path):
+        py_logger.error(f"Specified directory {dir_path} is missing")
+        raise NotADirectoryError(f"Specified directory {dir_path} is missing")
