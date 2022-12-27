1a717b4f24a287b4ddc12eba084bf1a149a88920
Aleksei Seleznev <ferfa2@rambler.ru>
2022-12-27 18:43:22 +0300

    replace soup from download.py to parse_resources.py

--- page_loader/parse_resources.py
+++ page_loader/parse_resources.py
@@ -1,8 +1,12 @@
 from page_loader.naming import name_res_in_html_file
+from bs4 import BeautifulSoup
 
 
 # flake8: noqa: C901
-def parse_resources(url, tags):
+def parse_resources(url, file):
+    soup = BeautifulSoup(file, 'html.parser')
+    tags = soup.find_all(['img', 'link', 'script'])
+
     resources_for_download = []
     for tag in tags:
         if tag.name == 'img':
@@ -20,4 +24,5 @@ def parse_resources(url, tags):
             if link_path is not None:
                 tag['src'] = name_res_in_html_file(url, link_path)
                 resources_for_download.append(link_path)
-    return resources_for_download
+    text_result = soup.prettify()
+    return resources_for_download, text_result
